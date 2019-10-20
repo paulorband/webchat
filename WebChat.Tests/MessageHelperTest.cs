edbb4c454985c1eaed7826e865044f5b2b9d155c
Paulo Bandeira <paulorband@gmail.com>
2019-10-19 22:18:23 -0300

    Projeto de testes

---
+++ WebChat.Tests/MessageHelperTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebChat.WebChat;
+
+namespace WebChat.Tests
+{
+	[TestClass]
+	public class MessageHelperTest
+	{
+		[TestMethod]
+		public void Quando_Uma_Mesagem_Tiver_Uma_Citacao_Privada_Deve_Ser_Possivel_Definir_O_Usuario_De_Destino()
+		{
+			var userMessage = "/p @jose Podemos conversar?";
+
+			MessageHelper.CheckForPrivateMessage(userMessage, out string receiverUserName, out _);
+
+			Assert.AreEqual("jose", receiverUserName);
+		}
+
+		[TestMethod]
+		public void Quando_Uma_Mesagem_Tiver_Uma_Citacao_Privada_Deve_Ser_Possivel_Definir_A_Mensagem_Sem_A_Citacao()
+		{
+			var userMessage = "/p @jose Podemos conversar?";
+
+			MessageHelper.CheckForPrivateMessage(userMessage, out _, out string textMessage);
+
+			Assert.AreEqual("Podemos conversar?", textMessage);
+		}
+
+		[TestMethod]
+		public void Quando_Uma_Mesagem_Tiver_Uma_Citacao_Publica_Deve_Ser_Possivel_Definir_O_Usuario_Citado()
+		{
+			var userMessage = "@jose Podemos conversar?";
+
+			MessageHelper.CheckForTaggedMessage(userMessage, out string receiverUserName, out _);
+
+			Assert.AreEqual("jose", receiverUserName);
+		}
+
+		[TestMethod]
+		public void Quando_Uma_Mesagem_Tiver_Uma_Citacao_Publica_Deve_Ser_Possivel_Definir_A_Mensagem_Sem_A_Citacao()
+		{
+			var userMessage = "@jose Podemos conversar?";
+
+			MessageHelper.CheckForTaggedMessage(userMessage, out _, out string textMessage);
+
+			Assert.AreEqual("Podemos conversar?", textMessage);
+		}
+	}
+}
---
+++ WebChat.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebChat.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebChat.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4760312f-6228-4f63-b68e-75c948684acb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
